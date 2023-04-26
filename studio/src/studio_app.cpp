16d31887b3751944511fa7c7d5cea7234631eb4a
Levi Armstrong <levi.armstrong@gmail.com>
2023-04-26 11:59:38 -0500

    Add studio application

---
+++ studio/src/studio_app.cpp
@@ -0,0 +1,31 @@
+#include <tesseract_qt/studio/studio.h>
+#include <tesseract_qt/common/theme_utils.h>
+#include <QApplication>
+#include <QDebug>
+
+int main(int argc, char* argv[])
+{
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#if QT_VERSION >= 0x050600
+  QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+#endif
+  QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+
+  QApplication app(argc, argv);
+  QApplication::setApplicationName("Tesseract Studio");
+  QApplication::setQuitOnLastWindowClosed(true);
+
+  Q_INIT_RESOURCE(tesseract_qt_resources);
+  Q_INIT_RESOURCE(qdarkstyle_dark);
+  Q_INIT_RESOURCE(qdarkstyle_light);
+
+  // setup stylesheet
+  app.setStyleSheet(tesseract_gui::themes::getDarkTheme());
+
+  tesseract_gui::Studio widget;
+  widget.show();
+
+  return QApplication::exec();
+}
